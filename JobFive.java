cf6ce4e93b34d6df074242a0138d9d83bc34937c
Rel <72431760+RelentlessReboot@users.noreply.github.com>
2020-12-21 03:24:42 -0500

    Got Task 2 working in JobFive.java

--- JobFive.java
+++ JobFive.java
@@ -8,12 +8,14 @@ import java.io.IOException;
 public class JobFive {
     public static void spyNeighbors(String subnet) throws IOException {
         int timeout = 1000;
-
+        System.out.println("NOTE:\tYou can only find the MAC Address of the localhost using Java, not those of remote hosts in the same network."
+        + "\n\tThat means that JobSeeker.java cannot report the MAC addresses of other live hosts sharing its LAN, it can only report their IP addresses.\n");
         for (int i = 1; i < 255; i++) {
             String host = subnet + "." + i;//get ip address
 
             if (InetAddress.getByName(host).isReachable(timeout)) {
-                InetAddress otherHost = InetAddress.getByName(host);
+                //code to find MAC address of given host IP
+                /*InetAddress otherHost = InetAddress.getByName(host);
                 
                 NetworkInterface otherNet = NetworkInterface.getByInetAddress(otherHost);
 
@@ -23,8 +25,8 @@ public class JobFive {
                 for (int j = 0; j < onetAddr.length; j++) {
                     ohex[j] = String.format("%02X", onetAddr[i]);
                 }
-                String otherMac = String.join("-", ohex);
-                System.out.println("Host " + i + " with IP Address " + host + " and MAC Address " + otherMac + " is on the same LAN as JobSeeker.");
+                String otherMac = String.join("-", ohex);*/
+                System.out.println("Host IP " + host + " is on the same LAN as JobSeeker.");
             }
         }
     }
