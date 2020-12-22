ce86b54fa2427f8366850bb199e08ffc2c089938
Rel <72431760+RelentlessReboot@users.noreply.github.com>
2020-12-22 16:04:42 -0500

    JobCreator now stores output to file output.txt

--- FPJobCreator.java
+++ FPJobCreator.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.net.Socket;
 import java.net.*;
 import java.io.*;
@@ -16,14 +19,16 @@ public class FPJobCreator {
         int[] servers = {4999, 4998, 4997};//allows 3 different sockets for 3 different JobSeekers
         int i = 0;
         int count = 0;
-        in maxTries = 3;
+        int maxTries = 3;
         //loop for multiple JobSeekers
         while (i < 3 && count < maxTries) {
             try {
-                System.out.println("Inside while loop.\n");
-                
+                //prep to store in file
+                PrintStream output = new PrintStream(new FileOutputStream("output.txt"));
+
                 Socket s = new Socket("localhost", servers[i]);
                 System.out.println("Socket connected.");
+                output.println("Socket connected.");
                 PrintWriter pr = new PrintWriter(s.getOutputStream());
                 Scanner sc = new Scanner(System.in);
                 Scanner sc2 = new Scanner(System.in);
@@ -38,25 +43,39 @@ public class FPJobCreator {
                     + "5 = Traceroute between JobSeeker and another node in the network. Find the nearest JobSeeker(s) to the target node. \n"
                     + "6 = Execute an ICMP flood attack against a port at an IP address \n"
                     + "\t\t Warning: Doing this against a valid IP address may count as an illegal DDoS attack. Please use dummy IP 0.0.0.0. \n");
+                output.println("=== MENU === \n"
+                        + "1 = Detect if an IP address/host name is online \n"
+                        + "2 = Detect the status of a port at an IP address \n"
+                        + "3 = Find JobSeekers on the same LAN as JobCreator. Report IP and MAC addresses of all live hosts sharing JobSeeker's LAN. \n"
+                        + "4 = Execute a TCP flood attack against a port at an IP address \n"
+                        + "\t\t Warning: Doing this against a valid IP address may count as an illegal DDoS attack. Please use dummy IP 0.0.0.0 \n"
+                        + "5 = Traceroute between JobSeeker and another node in the network. Find the nearest JobSeeker(s) to the target node. \n"
+                        + "6 = Execute an ICMP flood attack against a port at an IP address \n"
+                        + "\t\t Warning: Doing this against a valid IP address may count as an illegal DDoS attack. Please use dummy IP 0.0.0.0. \n");
                 System.out.print("Job Type: ");
+                output.print("Job Type: ");
                 jobType = sc.nextLine();
                 pr.println(jobType);
                 pr.flush();
                 //get ip address from user
                 if (!jobType.equalsIgnoreCase("3")) {
                     System.out.print("Enter an IP Address/host name: ");
+                    output.print("Enter an IP Address/host name: ");
                     ip = sc2.nextLine();
                 }
                 else {
                     System.out.print("JobSeeker IP Address: ");
+                    output.print("JobSeeker IP Address: ");
                     ip = (InetAddress.getLocalHost().getHostAddress()).trim();//local IP address
                     System.out.println(ip + " (localhost)");
+                    output.println(ip + " (localhost)");
                 }
                 pr.println(ip);
                 pr.flush();
                 //If job type is not 1, 3 or 5, get port number from user
                 if (!jobType.equalsIgnoreCase("1") && !jobType.equalsIgnoreCase("3") && !jobType.equalsIgnoreCase("5")) {
                     System.out.println("Enter a port number:");
+                    output.println("Enter a port number:");
                     port = sc3.nextInt();
                     pr.println(port);
                     pr.flush();
@@ -67,18 +86,23 @@ public class FPJobCreator {
 
                 String str = bf.readLine();
                 System.out.println("JobSeeker: " + str + "\n");
+                output.println("JobSeeker: " + str + "\n");
 
                 str = bf.readLine();
                 System.out.println("JobSeeker: " + str);
+                output.println("JobSeeker: " + str);
 
                 str = bf.readLine();
                 System.out.println(str);
+                output.println(str);
 
                 int jobDoneNum = bf.read();
                 if (jobDoneNum == 1) {
                     isJobDone = true;
                 }
                 count++;
+                //close file
+                output.close();
             } catch (IOException e) {
                 System.out.println("Connection failed. Abort");
             }
